1dd43e06c28450e6e70b0abfc8b696bb2daab02d
Josh Sample <joshuawsample@gmail.com>
2018-03-04 01:41:59 -0500

    Connected add to order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,43 +1,62 @@
-import React, { Component } from 'react';
-import Header from './Header'
-import Order from './Order'
-import Inventory from './Inventory'
-import sampleFishes from '../sample-fishes'
+import React, { Component } from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends Component {
-    state = {
-        fishes: {},
-        order: {}
-    }
-    
-    addFish = (fish) => {
-        //1. Making a copy of existing state
-        const fishes = {...this.state.fishes}
-        //2. Add our new fish to that fishes variable
-        fishes[`fish${Date.now()}`] = fish
-        //3. Set the new fishes object to state
-        this.setState({fishes})    
-    }
+  state = {
+    fishes: {},
+    order: {}
+  };
 
-    loadSampleFishes = () => {
-        this.setState({fishes:sampleFishes})
-    }
-    render() {
+  addFish = fish => {
+    //1. Making a copy of existing state
+    const fishes = { ...this.state.fishes };
+    //2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    //3. Set the new fishes object to state
+    this.setState({ fishes });
+  };
 
-        return (
-           <div className="catch-of-the-day">
-            <div className="menu">
-            <Header tagline="Fresh Seafood Market" />
-            
-            </div>
-            <Order />
-            <Inventory
-            addFish={this.addFish}
-            loadSampleFishes={this.loadSampleFishes}
-            />
-           </div>
-        );
-    }
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = key => {
+    //1. Make a copy of state
+    const order = { ...this.state.order };
+    //2. Either add to the order, or update number in order
+    order[key] = order[key] + 1 || 1;
+    //3. Call setState to update our order
+    this.setState({ order });
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
+        </div>
+        <Order />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
